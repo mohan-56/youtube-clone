0d2c8fea0f909634e0a2f314b5304137f9ef8b40
mohan56 <polamarasettymohan56@gmail.com>
2024-03-22 00:01:29 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+// import {box} from '@mui/material'
+
+import './App.css'
+import Navbar from './components/navbar'
+function App() {
+ 
+
+  return (
+   <>
+   <BrowserRouter>
+   <Navbar/>
+   </BrowserRouter>
+    
+   </>
+    
+  
+   
+
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Stack} from '@mui/material'
+import {Link} from "react-router-dom";
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+const Navbar = () => {
+  return (
+    <YouTubeIcon/>
+  )
+}
+
+export default Navbar
